8d076befb815bf08c39bd85c6670d23469215759
bentondrew <benton.s.drew@drewantech.com>
2019-01-10 16:49:45 -0600

    Fix typo in index json encoding error

--- app/index/index.go
+++ app/index/index.go
@@ -11,7 +11,7 @@ import (
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json+problem")
   w.WriteHeader(http.StatusNotFound)
-  if err := json.NewEncoder(w).Encode(problem.Problem{Status: 404, Title: "Not Found", Detail: fmt.Sprintf("%s not found", r.URL), Type: "about:blank"}) err != nil {
+  if err := json.NewEncoder(w).Encode(problem.Problem{Status: 404, Title: "Not Found", Detail: fmt.Sprintf("%s not found", r.URL), Type: "about:blank"}); err != nil {
     panic(err)
   }
 }
