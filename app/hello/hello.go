df824f8b54bf6409b3ddc473dbf1229c7ab0fdec
bentondrew <benton.s.drew@drewantech.com>
2019-01-14 16:02:21 -0600

    Add all origins to hello endpoint.

--- app/hello/hello.go
+++ app/hello/hello.go
@@ -8,6 +8,7 @@ import (
 
 func HandleHello(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
   w.WriteHeader(http.StatusOK)
   if err := json.NewEncoder(w).Encode("Hello World!"); err != nil {
     panic(err)
