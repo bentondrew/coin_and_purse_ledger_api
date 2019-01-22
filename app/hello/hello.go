68ad2342fec68af3c6bedbce134afb63afd319bd
bentondrew <benton.s.drew@drewantech.com>
2019-01-21 22:17:18 -0600

    Catch json marshal errors.

--- app/hello/hello.go
+++ app/hello/hello.go
@@ -10,5 +10,11 @@ func HandleHello(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   w.Header().Set("Access-Control-Allow-Origin", "*")
   w.WriteHeader(http.StatusOK)
-  w.Write(json.Marshal("Hello World!"))
+  b, err = json.Marshal("Hello World!")
+  if err != nil {
+    panic(err) 
+  }
+  else {
+    w.Write(b)
+  }
 }
