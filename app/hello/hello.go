aded03f1fa19dfe35080d70bc3d27040f27fcbc5
bentondrew <benton.s.drew@drewantech.com>
2019-01-21 22:13:30 -0600

    Switch endpoint to use json marshal vs json.encoder.encode

--- app/hello/hello.go
+++ app/hello/hello.go
@@ -10,7 +10,5 @@ func HandleHello(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   w.Header().Set("Access-Control-Allow-Origin", "*")
   w.WriteHeader(http.StatusOK)
-  if err := json.NewEncoder(w).Encode("Hello World!"); err != nil {
-    panic(err)
-  }
+  w.Write(json.Marshal("Hello World!"))
 }
--- app/index/index.go
+++ app/index/index.go
@@ -11,7 +11,5 @@ import (
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/problem+json")
   w.WriteHeader(http.StatusNotFound)
-  if err := json.NewEncoder(w).Encode(problem.Problem{Status: 404, Title: "Not Found", Detail: fmt.Sprintf("%s not found", r.URL), Type: "about:blank"}); err != nil {
-    panic(err)
-  }
+  w.Write(json.Marshal(problem.Problem{Status: 404, Title: "Not Found", Detail: fmt.Sprintf("%s not found", r.URL), Type: "about:blank",}))
 }
