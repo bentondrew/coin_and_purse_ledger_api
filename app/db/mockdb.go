0d7562f8e323dd79aeec915af7d657c92c0dfd9a
bentondrew <benton.s.drew@drewantech.com>
2019-01-31 17:12:50 -0600

    Get logger from db in mockdb.

--- app/db/mockdb.go
+++ app/db/mockdb.go
@@ -29,7 +29,7 @@ func (db *DB) Find(out interface{}, where ...interface{}) *DB {
   if v, ok := out.(*[]transaction.Transaction); ok {
     append(&out, transaction.Transaction{ID: 1, Timestamp: "2019-01-30T03:17:41.12004Z", Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: "2019-01-30T19:41:10.421617Z", Amount: -5,})
   } else {
-    logger.Println("Mockdb Find does not recognize type of provided out.")
+    db.logger.Println("Mockdb Find does not recognize type of provided out.")
   }
   return db
 }
