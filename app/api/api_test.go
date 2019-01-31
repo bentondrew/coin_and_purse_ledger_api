b5f1e8ace2f41e1fac0242735d149519582b716c
bentondrew <benton.s.drew@drewantech.com>
2019-01-31 11:34:23 -0600

    Fix slice declaration in api test code.

--- app/api/api_test.go
+++ app/api/api_test.go
@@ -59,7 +59,7 @@ func TestEndpoints(t *testing.T) {
       out: httptest.NewRecorder(),
       handlerFunc: api.HandleGetAllTransactions,
       expectedStatus: http.StatusOK,
-      expectedBody: string(generateJsonByteArray([transaction.Transaction{ID: 1, Timestamp: "2019-01-30T03:17:41.12004Z", Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: "2019-01-30T19:41:10.421617Z", Amount: -5,}])[:]),
+      expectedBody: string(generateJsonByteArray([]transaction.Transaction{transaction.Transaction{ID: 1, Timestamp: "2019-01-30T03:17:41.12004Z", Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: "2019-01-30T19:41:10.421617Z", Amount: -5,}})[:]),
     },
     {
       name: "transactions_post",
