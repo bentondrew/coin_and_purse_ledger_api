2298d60eb42cbeac2730baeed26584fa2bef32e9
bentondrew <benton.s.drew@drewantech.com>
2019-01-31 17:30:01 -0600

    Add time parse err handling to mockdb

--- app/db/mockdb.go
+++ app/db/mockdb.go
@@ -28,7 +28,15 @@ func NewMockDatabase(logger *log.Logger) *DB {
 
 func (db *DB) Find(out interface{}, where ...interface{}) *DB {
   if v, ok := out.(*[]transaction.Transaction); ok {
-    append(&out, transaction.Transaction{ID: 1, Timestamp: time.Parse(time.RFC3339, "2019-01-30T03:17:41.12004Z"), Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: time.Parse(time.RFC3339, "2019-01-30T19:41:10.421617Z"), Amount: -5,})
+    t1, err := time.Parse(time.RFC3339, "2019-01-30T03:17:41.12004Z")
+    if err != nil {
+      panic(err) 
+    }
+    t2, err := time.Parse(time.RFC3339, "2019-01-30T19:41:10.421617Z")
+    if err != nil {
+      panic(err) 
+    }
+    append(&out, transaction.Transaction{ID: 1, Timestamp: t1, Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: t2, Amount: -5,})
   } else {
     db.logger.Println("Mockdb Find does not recognize type of provided out.")
   }
