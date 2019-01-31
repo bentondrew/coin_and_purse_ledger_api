9e08e66ba49be2efae17dcf8ad74a013d0882a02
bentondrew <benton.s.drew@drewantech.com>
2019-01-31 17:50:02 -0600

    Try to add values to Find interface in mockdb with reflect package.

--- app/db/mockdb.go
+++ app/db/mockdb.go
@@ -4,6 +4,7 @@ package db
 import (
   "log"
   "time"
+  "reflect"
   "github.com/Drewan-Tech/coin_and_purse_ledger_service/app/transaction"
 )
 
@@ -27,17 +28,23 @@ func NewMockDatabase(logger *log.Logger) *DB {
 
 
 func (db *DB) Find(out interface{}, where ...interface{}) *DB {
-  if v, ok := out.(*[]transaction.Transaction); ok {
-    t1, err := time.Parse(time.RFC3339, "2019-01-30T03:17:41.12004Z")
-    if err != nil {
-      panic(err) 
+  switch reflect.TypeOf(out).Kind() {
+  case reflect.Slice:
+    if v, ok := reflect.TypeOf(out).Elem().(transaction.Transaction); ok {
+      s := reflect.ValueOf(out)
+      t1, err := time.Parse(time.RFC3339, "2019-01-30T03:17:41.12004Z")
+      if err != nil {
+        panic(err) 
+      }
+      t2, err := time.Parse(time.RFC3339, "2019-01-30T19:41:10.421617Z")
+      if err != nil {
+        panic(err) 
+      }
+      append(s, transaction.Transaction{ID: 1, Timestamp: t1, Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: t2, Amount: -5,})
+    } else {
+      db.logger.Println("Mockdb Find does not handle the provide slice.")
     }
-    t2, err := time.Parse(time.RFC3339, "2019-01-30T19:41:10.421617Z")
-    if err != nil {
-      panic(err) 
-    }
-    append(&out, transaction.Transaction{ID: 1, Timestamp: t1, Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: t2, Amount: -5,})
-  } else {
+  default:
     db.logger.Println("Mockdb Find does not recognize type of provided out.")
   }
   return db
