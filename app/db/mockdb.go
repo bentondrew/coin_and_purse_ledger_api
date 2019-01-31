1c17bd0df47d0735a8722eb2384e8848ab8ce06d
bentondrew <benton.s.drew@drewantech.com>
2019-01-31 17:00:40 -0600

    Fix return from mockdb.

--- app/db/mockdb.go
+++ app/db/mockdb.go
@@ -20,5 +20,5 @@ func (db *gorm.DB) Find(out interface{}, where ...interface{}) *gorm.DB {
   if v, ok := out.(*[]transaction.Transaction); ok {
     append(&listToPopulate, transaction.Transaction{ID: 1, Timestamp: "2019-01-30T03:17:41.12004Z", Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: "2019-01-30T19:41:10.421617Z", Amount: -5,})
   }
-  retun db
+  return db
 }
