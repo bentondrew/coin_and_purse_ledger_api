4f8fd408d12f27e60fbfa3c2ea5e09e07cf34a9e
bentondrew <benton.s.drew@drewantech.com>
2019-02-01 11:55:10 -0600

    Update type checking in mockdb find

--- app/db/mockdb.go
+++ app/db/mockdb.go
@@ -30,22 +30,27 @@ func NewMockDatabase(logger *log.Logger) *DB {
 func (db *DB) Find(out interface{}, where ...interface{}) *DB {
   switch reflect.TypeOf(out).Kind() {
   case reflect.Slice:
-    if v, ok := reflect.TypeOf(out).Elem().(transaction.Transaction); ok {
-      s := reflect.ValueOf(out)
-      t1, err := time.Parse(time.RFC3339, "2019-01-30T03:17:41.12004Z")
-      if err != nil {
-        panic(err) 
-      }
-      t2, err := time.Parse(time.RFC3339, "2019-01-30T19:41:10.421617Z")
-      if err != nil {
-        panic(err) 
-      }
-      append(s, transaction.Transaction{ID: 1, Timestamp: t1, Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: t2, Amount: -5,})
-    } else {
-      db.logger.Println("Mockdb Find does not handle the provide slice.")
+    switch reflect.TypeOf(out).Elem().Kind() {
+    case reflect.Struct:
+      switch reflect.TypeOf(out).Elem().Name() {
+      case transaction.Transaction:
+        t1, err := time.Parse(time.RFC3339, "2019-01-30T03:17:41.12004Z")
+        if err != nil {
+          panic(err) 
+        }
+        t2, err := time.Parse(time.RFC3339, "2019-01-30T19:41:10.421617Z")
+        if err != nil {
+          panic(err) 
+        }
+        append(reflect.TypeOf(out).Elem(), transaction.Transaction{ID: 1, Timestamp: t1, Amount: 10,}, transaction.Transaction{ID: 1, Timestamp: t2, Amount: -5,})
+      default:
+        db.logger.Println("Mockdb Find currently only handles struct of type transaction.Transaction.")
+    default:
+      db.logger.Println("Mockdb Find currently only handles struct in slice.")
+    }
     }
   default:
-    db.logger.Println("Mockdb Find does not recognize type of provided out.")
+    db.logger.Println("Mockdb Find currently only handles slice.")
   }
   return db
 }
