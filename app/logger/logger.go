7a41009af2e4c62f69e57505a987a2d43b2daa0d
bentondrew <benton.s.drew@drewantech.com>
2019-04-08 14:33:29 -0500

    Delint logger module.

--- app/logger/logger.go
+++ app/logger/logger.go
@@ -5,7 +5,7 @@ import (
   "os"
 )
 
-
+/*NewLogger returns an instance of a logger configured for this service.*/
 func NewLogger() *log.Logger{
   return log.New(os.Stdout, "ledger_service ", log.LstdFlags|log.Lshortfile)
 }
