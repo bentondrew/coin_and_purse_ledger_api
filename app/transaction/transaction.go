60a433465bde34b03b12cbe9a5a9a45414e80e69
bentondrew <benton.s.drew@drewantech.com>
2019-04-08 11:59:05 -0500

    Delint transaction module.

--- app/transaction/transaction.go
+++ app/transaction/transaction.go
@@ -6,6 +6,12 @@ import (
 )
 
 
+/*Transaction struct is the model for transaction
+instances in the ledger service. Incoming and
+outgoing JSON transactions details are converted to
+and from this model. This model is used to define
+the database table and model instances are used
+for reading and writing rows to/from the database.*/
 type Transaction struct {
   ID uuid.UUID `json:"id" gorm:"type:uuid;primary_key"`
   Timestamp time.Time `json:"timestamp"`
