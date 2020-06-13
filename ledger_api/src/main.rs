acef30091e8a6d5f32c55b424dad450c1e4c262f
Benton Drew <benton.s.drew@drewantech.com>
2020-06-13 00:16:08 -0500

    Fix use of calling year, month and day in string print of today's utc time

--- ledger_api/src/main.rs
+++ ledger_api/src/main.rs
@@ -1,13 +1,13 @@
 // Rust tutorial
 // https://doc.rust-lang.org/rust-by-example/hello/print.html
-use chrono::{Local, Utc};
+use chrono::{Utc, Datelike};
 
 #[derive(Debug)]
 struct TestStructure(i32);
 
 fn main() {
     println!("Hello, {name}!", name="Benton");
-    let now = Utc::now()
+    let now = Utc::today();
     println!("Today's date in utc: {year:>04}/{month:>02}/{day:>02}", year=now.year(), month=now.month(), day=now.day());
     println!("{:?}", TestStructure(7));
 }
