495abb2ef73a2bc53cebc26a7c9d0eb01f87605c
Benton Drew <benton.s.drew@drewantech.com>
2020-06-12 23:46:34 -0500

    Print utc and local datetime

--- ledger_api/src/main.rs
+++ ledger_api/src/main.rs
@@ -1,6 +1,14 @@
 // Rust tutorial
 // https://doc.rust-lang.org/rust-by-example/hello/print.html
+use chrono::{Local, Utc};
+
+#[derive(Debug)]
+struct TestStructure(i32);
+
 fn main() {
     println!("Hello, {name}!", name="Benton");
-    println!("Today's date: {year:>04}, {name}!", name="Benton");
+    println!("{:?}", Utc::now());
+    println!("{:?}", Local::now());
+    println!("Today's date in utc: {year:>04}/{day:>02}/{month:>02}", year=2020, day=6, month=6);
+    println!("{:?}", TestStructure(7));
 }
