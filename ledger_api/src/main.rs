6689d50c81cdec7ad96ae5f60c6f1960c3face7d
Benton Drew <benton.s.drew@drewantech.com>
2020-06-13 00:27:27 -0500

    Add a display for a structure

--- ledger_api/src/main.rs
+++ ledger_api/src/main.rs
@@ -1,13 +1,23 @@
 // Rust tutorial
 // https://doc.rust-lang.org/rust-by-example/hello/print.html
+use std::fmt;
 use chrono::{Utc, Datelike};
 
 #[derive(Debug)]
-struct TestStructure(i32);
+struct DebugStructure(i32);
+
+struct DisplayStructure(i32);
+
+impl fmt::Display for DisplayStructure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 fn main() {
     println!("Hello, {name}!", name="Benton");
     let now = Utc::today();
     println!("Today's date in utc: {year:>04}/{month:>02}/{day:>02}", year=now.year(), month=now.month(), day=now.day());
-    println!("{:?}", TestStructure(7));
+    println!("Display: {}", DisplayStructure(3));
+    println!("Debug: {:?}", DebugStructure(7));
 }
