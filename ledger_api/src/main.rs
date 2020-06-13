bc527adf3b30df9d47b8f96b814a771320495fa1
Benton Drew <benton.s.drew@drewantech.com>
2020-06-12 23:51:17 -0500

    Convert print of date to use datetime.

--- ledger_api/src/main.rs
+++ ledger_api/src/main.rs
@@ -7,8 +7,7 @@ struct TestStructure(i32);
 
 fn main() {
     println!("Hello, {name}!", name="Benton");
-    println!("{:?}", Utc::now());
-    println!("{:?}", Local::now());
-    println!("Today's date in utc: {year:>04}/{day:>02}/{month:>02}", year=2020, day=6, month=6);
+    let now = Utc::now()
+    println!("Today's date in utc: {year:>04}/{month:>02}/{day:>02}", year=now.year(), month=now.month(), day=now.day());
     println!("{:?}", TestStructure(7));
 }
