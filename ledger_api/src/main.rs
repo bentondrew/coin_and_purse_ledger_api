52060d2804ab9126c39a0bf2dc8074b5d265c667
Benton Drew <benton.s.drew@drewantech.com>
2020-06-14 11:31:51 -0500

    Add display for a list (use of ?)

--- ledger_api/src/main.rs
+++ ledger_api/src/main.rs
@@ -1,5 +1,5 @@
 // Rust tutorial
-// https://doc.rust-lang.org/rust-by-example/hello/print.html
+// https://doc.rust-lang.org/rust-by-example/hello/print/print_display/testcase_list.html
 use std::fmt;
 use chrono::{Utc, Datelike};
 
@@ -14,10 +14,27 @@ impl fmt::Display for DisplayStructure {
     }
 }
 
+struct ListStructure(Vec<i32>);
+
+impl fmt::Display for ListStructure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Get vector out of struct
+        let vec = &self.0;
+        write!(f, "[")?;
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+        write!(f, "]")
+    }
+}
+
 fn main() {
     println!("Hello, {name}!", name="Benton");
     let now = Utc::today();
     println!("Today's date in utc: {year:>04}/{month:>02}/{day:>02}", year=now.year(), month=now.month(), day=now.day());
     println!("Display: {}", DisplayStructure(3));
     println!("Debug: {:?}", DebugStructure(7));
+    let vs = ListStructure(vec![1, 2, 3]);
+    println!("{}", vs);
 }
