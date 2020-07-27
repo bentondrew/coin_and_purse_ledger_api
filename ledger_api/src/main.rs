4948a9f2b6bee9cd00dd2469f7617332b083a938
Benton Drew <benton.s.drew@drewantech.com>
2020-07-27 10:48:13 -0500

    Add rust formatting tutorial.

--- ledger_api/src/main.rs
+++ ledger_api/src/main.rs
@@ -1,5 +1,5 @@
 // Rust tutorial
-// https://doc.rust-lang.org/rust-by-example/hello/print/print_display/testcase_list.html
+// https://doc.rust-lang.org/rust-by-example/hello/print/fmt.html
 use std::fmt;
 use chrono::{Utc, Datelike};
 
@@ -29,6 +29,36 @@ impl fmt::Display for ListStructure {
     }
 }
 
+#[derive(Debug)]
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl fmt::Display for City {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 {'N'}  else {'S'};
+        let lon_c = if self.lat >= 0.0 {'E'}  else {'W'};
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {} {:#04X}{:02X}{:02X})",
+               self.red, self.green, self.blue, self.red, self.green, self.blue)
+    }
+}
+
 fn main() {
     println!("Hello, {name}!", name="Benton");
     let now = Utc::today();
@@ -37,4 +67,18 @@ fn main() {
     println!("Debug: {:?}", DebugStructure(7));
     let vs = ListStructure(vec![1, 2, 3]);
     println!("{}", vs);
+    for city in [
+        City {name: "Dublin", lat: 53.347778, lon: -6.259722},
+        City {name: "Oslo", lat: 59.95, lon: 10.75},
+        City {name: "Vancouver", lat: 49.25, lon: -123.1},
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color {red: 128, green: 255, blue: 90},
+        Color {red: 0, green: 3, blue: 254},
+        Color {red: 0, green: 0, blue: 0},
+    ].iter() {
+        println!("{}", *color);
+    }
 }
